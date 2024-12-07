817f3667ef4ad73fc1c08afc3da3041d77ae1409
Ilse <ilse.smits.97@gmail.com>
2024-12-07 14:45:55 +0100

    Update Day1.cs

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -1,10 +1,27 @@
-﻿namespace Days
+﻿using System;
+using System.Collections.Immutable;
+using System.Reflection.Metadata;
+
+namespace Days
 {
     public class Day1 : IDay
     {
         public int AnswerA(string pathInput)
         {
-            throw new NotImplementedException();
+            IEnumerable<string> inputLines= File.ReadLines(pathInput);
+            List<int> listOne = new List<int>();
+            List<int> listTwo = new List<int>();
+
+            foreach (string line in inputLines)
+            {
+                // TODO
+            }
+
+            listOne.Sort();
+            listTwo.Sort();
+            int sum = listOne.Zip(listTwo, (x, y) => Math.Abs(x - y)).Sum();
+            
+            return sum;
         }
 
         public int AnswerB(string pathInput)
